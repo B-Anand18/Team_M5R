586de9395abfe89f476f196ee9498a2ca04cb882
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2022-09-03 23:48:37 +0530

    about page basic done by kartikey ;)

--- Django_Blog_App-main/blog_v2/Blog/views.py
+++ Django_Blog_App-main/blog_v2/Blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
 from django.urls import reverse
 from .models import Post, Comments
 from django.contrib.auth.models import User
@@ -6,7 +6,6 @@ from django.contrib.auth.decorators import login_required
 from .forms import PostForm, CommentsForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from functools import wraps
-
 
 
 def home(request, pk='latest'):
@@ -27,33 +26,33 @@ def home(request, pk='latest'):
         page_obj = p.page(1)
     except EmptyPage:
         page_obj = p.page(p.num_pages)
-    return render(request, 'Blog/home.html',{'page_obj': page_obj, 'popular' : popular, 'latest' : latest})
+    return render(request, 'Blog/home.html', {'page_obj': page_obj, 'popular': popular, 'latest': latest})
 
 
-def Post_view(request,pk):
+def Post_view(request, pk):
     post = Post.objects.get(id=pk)
-    comments = Comments.objects.filter(post= pk).order_by('-date')
+    comments = Comments.objects.filter(post=pk).order_by('-date')
     if request.method == 'POST':
         form = CommentsForm(request.POST)
         if form.is_valid():
             form.instance.author = request.user
             form.instance.post = post
             form.save()
-            return redirect('view_post', pk = pk)
+            return redirect('view_post', pk=pk)
     else:
         form = CommentsForm()
-    return render(request, 'Blog/post.html',{'post': post, 'comments': comments,'form': form})
+    return render(request, 'Blog/post.html', {'post': post, 'comments': comments, 'form': form})
 
 
-def delete_comment(request):    
-    comment = Comments.objects.get(id = request.GET['pk'])
+def delete_comment(request):
+    comment = Comments.objects.get(id=request.GET['pk'])
     comment.delete()
-    return HttpResponse("succes")    
+    return HttpResponse("succes")
 
 
-def user_profile(request,pk):
-    author = User.objects.get(username = pk)
-    posts = Post.objects.filter(author = author).order_by('-date')
+def user_profile(request, pk):
+    author = User.objects.get(username=pk)
+    posts = Post.objects.filter(author=author).order_by('-date')
     p = Paginator(posts, 6)
     page_number = request.GET.get('page')
     try:
@@ -65,10 +64,9 @@ def user_profile(request,pk):
     return render(request, 'Blog/profile.html', {'page_obj': page_obj, 'user': author})
 
 
-
 def categories(request, pk):
     category = str(pk)
-    posts = Post.objects.filter(category = pk).order_by('-date')
+    posts = Post.objects.filter(category=pk).order_by('-date')
     p = Paginator(posts, 6)
     page_number = request.GET.get('page')
     try:
@@ -80,7 +78,7 @@ def categories(request, pk):
     return render(request, 'Blog/category.html', {'page_obj': page_obj, 'category': category})
 
 
-@login_required(login_url = 'login')
+@login_required(login_url='login')
 def new_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
@@ -91,16 +89,16 @@ def new_post(request):
     else:
         form = PostForm()
     context = {'form': form}
-    return render(request,'Blog/new_post.html',context)
+    return render(request, 'Blog/new_post.html', context)
 
 
-@login_required(login_url = 'login')
-def update_post(request,pk):
+@login_required(login_url='login')
+def update_post(request, pk):
     post = Post.objects.get(id=pk)
-    
+
     if request.user != post.author:
         return HttpResponse("<h2>access denied!</h2>")
-    
+
     form = PostForm(instance=post)
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES, instance=post)
@@ -108,37 +106,37 @@ def update_post(request,pk):
             form.save()
             return redirect(reverse('view_post', kwargs={'pk': pk}))
     context = {'form': form, 'item': post}
-    return render(request,'Blog/update.html',context)
+    return render(request, 'Blog/update.html', context)
 
 
-@login_required(login_url = 'login')
-def delete(request,pk):
+@login_required(login_url='login')
+def delete(request, pk):
     post = Post.objects.get(id=pk)
     if request.user != post.author:
         return HttpResponse("<h2>access denied!</h2>")
     if request.method == 'POST':
         post.delete()
-        return redirect('profile', pk = request.user)
+        return redirect('profile', pk=request.user)
 
     context = {'post': post}
-    return render(request,'Blog/delete.html',context) 
+    return render(request, 'Blog/delete.html', context)
 
 
-def post_like(request):    
-    post = Post.objects.get(id= request.GET['pk'])
+def post_like(request):
+    post = Post.objects.get(id=request.GET['pk'])
     post.likes.add(request.user)
     return HttpResponse("succes")
-        
 
-def post_unlike(request):    
-    post = Post.objects.get(id= request.GET['pk'])
+
+def post_unlike(request):
+    post = Post.objects.get(id=request.GET['pk'])
     post.likes.remove(request.user)
     return HttpResponse("succes")
 
 
 def search(request):
     keyword = request.POST.get('word')
-    result = Post.objects.filter(title__contains = keyword)
+    result = Post.objects.filter(title__contains=keyword)
     p = Paginator(result, 5)
     page_number = request.GET.get('page')
     try:
@@ -147,8 +145,8 @@ def search(request):
         page_obj = p.page(1)
     except EmptyPage:
         page_obj = p.page(p.num_pages)
-    return render(request,'Blog/search_result.html',{'page_obj': page_obj, 'keyword': keyword})
+    return render(request, 'Blog/search_result.html', {'page_obj': page_obj, 'keyword': keyword})
 
 
-
-
+def about(request):
+    return render(request, 'Blog/about.html')
